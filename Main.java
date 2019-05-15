d29f5f27af60d2a3b5426d698c95949f2ba24d07
kertliinat <43129546+kertliinat@users.noreply.github.com>
2019-05-15 12:53:00 +0300

    Andri, Kert

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main{
+  public static void main(String[] args){
+    isikukood isikukood = new isikukood();
+    isikukood.read();
+  }
+
+
+
+
+}
---
+++ isikukood.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+
+
+public class isikukood{
+
+  public String read(){
+    System.out.println("Sisesta isikukood: ");
+    Scanner in = new Scanner(System.in);
+    String isikukood = in.nextLine();
+    System.out.println(isikukood);
+    return isikukood;
+  }
+
+
+  public String sugu(){
+      
+
+
+
+
+      return "0";
+  }
+}
